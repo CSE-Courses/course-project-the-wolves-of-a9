1c3567f6b0cfbff54f9e0a3bb9463c1a53919083
jkuang5 <kuang316@gmail.com>
2020-10-19 21:31:40 -0400

    Added a UserStockOwnership object that has a ForeignKey to the Django User class and to the Stock objects.
    Constructable by calling
    
    i.e
    User.stocks.create(ticker=Stock.objects.get(ticker="AAPL"),quantity=1)
    
    The UserStockOwnership object has
    - ticker (ForeignKey to Stock)
    - sharpe_ratio (DecimalField)
    - sortino_ratio (DecimalField)
    - user (ForeignKey to Django.contrib.auth.models.User)
    - alpha (DecimalField)
    - beta (DecimalField)
    
    Its toString function is to return ticker name + quantity.
    
    Furthermore, the Stock object now has multiple functions.
    The important ones are
       - update_alpha_and_beta
          This takes no constructors and pings Quandl to update the alpha and beta of the stock by comparing it with the S&P500. It updates self.alpha, self.beta and self.price
       - calculate_ratio(days, ratioType) where ratioType is either 'sharpe' or 'sortino'
          This assigns a sharpe/sortino ratio to the stock. The stock usually will have both, so self.sharpe_ratio and self.sortino_ratio will be updated according to the calculations of this.

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,41 +1,96 @@
 from django.db import models
+from django.contrib.auth.models import User
 import quandl
 import pandas as pd
 import numpy as np
 from pandas.plotting import scatter_matrix
-import random
-# Create your models here.
-
-from django.urls import reverse  # To generate URLS by reversing URL patterns
-
+from scipy import stats
+from django.urls import reverse
+from datetime import datetime
+api_key = ""
 
 class Stock(models.Model):
-    ticker = models.CharField(max_length=5)
+    ticker = models.CharField(max_length=5,unique=True)
     price = models.DecimalField(max_digits=8,decimal_places=2,null=True)
-    SharpeRatio = models.DecimalField(max_digits=8,decimal_places=2,null=True) #I have literally no clue what should be done to make a Sharpe Ratio.
-    SortinoRatio = models.DecimalField(max_digits=8,decimal_places=2,null=True) #Ditto.
-    date = models.DateTimeField(auto_now_add=True)
+    sharpe_ratio = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    sortino_ratio = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
     alpha = models.DecimalField(max_digits=10, decimal_places=8, null=True)
     beta = models.DecimalField(max_digits=10, decimal_places=8, null=True)
+    #updater = StockManager()
 
     def __str__(self):
         return self.ticker
 
-    def assignSharpeRatio(self,weights_array,log_ret):
-        weights_array = weights_array / np.sum(weights_array)
-        exp_ret = np.sum((log_ret.mean() * weights * 252))
-        exp_vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-        SR = exp_ret / exp_vol
-        self.SharpeRatio = SR
+    # Creates a column called 'get_returns' that consists of the percent changes of the closing products for each stock.
+    def get_returns(self, df1):
+        df1['returns'] = df1['Close'].pct_change(1)
+        return df1['returns']  # Returns one column.
+
+    def get_log_returns(self,close_df, column):
+        log_ret = np.log(close_df[column] / close_df[column].shift(1))
+        return log_ret
+
+    def construct_df(self,df_lst, column):
+        column_lst = []
+        for i in range(len(df_lst)):
+            column_lst.append(df_lst[i][str(column)])
+        stock_columns = pd.concat(column_lst, axis=1)
+        return stock_columns
+
+    def update_alpha_and_beta(self):
+        spy_data = quandl.get("eod/spy", authtoken=api_key,start_date="2019-10-19",end_date="2020-10-19")
+        stock_data = quandl.get("eod/{stock}".format(stock=str(self.ticker)), authtoken=api_key,start_date="2019-10-19",end_date="2020-10-19")
+        spy_returns = self.get_returns(spy_data)
+        stock_returns = self.get_returns(stock_data)
+        if len(stock_data['Open']) != 252:
+            print("Error: {Stock} is incomplete. Length of stock_data['Open'] is {number}".format(Stock=self.ticker, number = len(stock_data['Open'])))
+            return
+        self.beta, self.alpha, r_value, p_value, std_err = stats.linregress(
+            stock_returns.iloc[1:], spy_returns.iloc[1:])
+        self.price = stock_data['Close'][-1]
+        #self.date_updated = datetime.now()
+        self.save()
+        print("Alpha and Beta successfully updated! {Stock}:, Alpha: {alpha}, Beta: {beta}, Price: {price}".format(Stock=str(self.ticker),alpha=self.alpha,beta=self.beta, price= stock_data['Close'][-1]))
+
+    def calculate_ratio(self, days, ratioType):
+        stock_data = quandl.get("eod/{stock}".format(stock=str(self.ticker)), authtoken=api_key,
+                                start_date="2019-10-19", end_date="2020-10-19")
+        spy_data = quandl.get("eod/spy", authtoken=api_key, start_date="2019-10-19", end_date="2020-10-19")
+        close_df = stock_data["Close"]
+        log_ret = np.log(close_df / close_df.shift(1))
+        str(ratioType).lower()
+        exp_ret = np.sum((log_ret.mean() * days))
+        if ratioType == 'sharpe':
+            exp_vol = log_ret.std() * np.sqrt(days)
+            sharpe_ratio = exp_ret / exp_vol
+            print('The Sharpe Ratio is: {sharpe} '.format(sharpe=sharpe_ratio))
+            self.sharpe_ratio = sharpe_ratio
+            self.save()
+        elif ratioType == 'sortino':
+            exp_vol = log_ret[log_ret < 0].std() * np.sqrt(days)
+            sortino_ratio = exp_ret / exp_vol
+            print('The Sortino Ratio is: {sortino} '.format(sortino=sortino_ratio))
+            self.sortino_ratio = sortino_ratio
+            self.save()
+        else:
+            pass
 
 
+class UserStockOwnership(models.Model):
+    ticker = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    sharpe_ratio = models.DecimalField(max_digits=8,decimal_places=2,null=True)
+    sortino_ratio = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    alpha = models.DecimalField(max_digits=10, decimal_places=8, null=True)
+    beta = models.DecimalField(max_digits=10, decimal_places=8, null=True)
+    quantity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="stocks")
+
+    def __str__(self):
+        return "{ticker}: {quantitys}".format(ticker=self.ticker.ticker,quantitys=self.quantity)
 
 
-
-
-
-
-
+    #Define a way to calculate sharpe,sortino,alpha,beta of users portfolio.
 
 
 
@@ -93,7 +148,7 @@ class Book(models.Model):
 import uuid  # Required for unique book instances
 from datetime import date
 
-from django.contrib.auth.models import User  # Required to assign User as a borrower
+from django.contrib.auth.models import User, User, User  # Required to assign User as a borrower
 
 
 class BookInstance(models.Model):
